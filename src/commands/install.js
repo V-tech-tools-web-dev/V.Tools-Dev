9c94dd84b0946a5e60ca2324c75cf04adbe30a41
Slavko_V <slavko.vuletic92@gmail.com>
2021-07-18 20:57:08 +0200

    yea storm incoming...started install process/command cuz its gonna add itself to proper location once done...while installing more globals to use

--- src/commands/install.js
+++ src/commands/install.js
@@ -1,10 +1,29 @@
-const {Command, flags} = require('@oclif/command')
+const { Command, flags } = require('@oclif/command')
+const { exec } = require('child_process')
 
+function bashDo(inputCommand = '') {
+  exec(inputCommand, (err, stdout, stderr) => {
+    if (err) {
+      //some err occurred
+      console.error(err)
+    } else {
+      // the *entire* stdout and stderr (buffered)
+      console.log(`stdout: ${stdout}`)
+      console.log(`stderr: ${stderr}`)
+    }
+  })
+}
 class InstallCommand extends Command {
   async run() {
-    const {flags} = this.parse(InstallCommand)
+    const { flags } = this.parse(InstallCommand)
     const name = flags.name || 'world'
-    this.log(`hello ${name} from C:\\Users\\slavk\\V.Tools-DevSetup\\src\\commands\\install.js`)
+    this.log(
+      `hello ${name} from C:\\Users\\slavk\\V.Tools-DevSetup\\src\\commands\\install.js`
+    )
+    bashDo('PWD')
+    bashDo('echo "YEAAAA" ')
+    bashDo('ls -al')
+    bashDo('echo $PWD && cd .. && echo $PWD')
   }
 }
 
@@ -14,7 +33,7 @@ Extra documentation goes here
 `
 
 InstallCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
+  name: flags.string({ char: 'n', description: 'name to print' }),
 }
 
 module.exports = InstallCommand
